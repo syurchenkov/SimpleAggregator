f975de0b2097287a6e52b8940d02d99f2ccbb3ee
Slava Yurchenkov <yurchenkovslava@gmail.com>
2017-08-16 10:28:34 +0300

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  namespace :api do
+    
+  end
+  root 'layouts#index'
+  get '*path' => 'layouts#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
