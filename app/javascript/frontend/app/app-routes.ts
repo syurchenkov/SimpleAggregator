399aa5451d671b5a515cb3769dfa033cbf5a6f39
Slava Yurchenkov <yurchenkovslava@gmail.com>
2017-08-20 20:30:45 +0300

    Fix frontend routing

--- app/javascript/frontend/app/app-routes.ts
+++ app/javascript/frontend/app/app-routes.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './containers/home/home.component';
 import { LoginComponent } from './containers/login/login.component';
 
 export const routes: Routes = [
-{ path: '', pathMatch: 'full', redirectTo: 'home' }, 
-{ path: 'home', component: HomeComponent },
+{ path: '', component: HomeComponent },
 { path: 'login', component: LoginComponent }
 ]
